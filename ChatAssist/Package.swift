24a2167cde1232df7f8d0ca879aadb2a247b5e05
mustafakarakus <karakusmustafa@gmail.com>
2024-06-13 22:36:35 +0200

    Added Swift Package Manager and Cocoapods Configurations. Created the SDK and Example app structure.

---
+++ ChatAssist/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ChatAssist",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ChatAssist",
+            targets: ["ChatAssist"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ChatAssist"),
+        .testTarget(
+            name: "ChatAssistTests",
+            dependencies: ["ChatAssist"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
