e584261c678866bda90d34835130108ac42f4052
mustafakarakus <karakusmustafa@gmail.com>
2024-07-11 16:45:47 +0200

    update ui

--- ChatAssistExample/ChatAssistExample/ContentView.swift
+++ ChatAssistExample/ChatAssistExample/ContentView.swift
@@ -13,27 +13,37 @@ struct ContentView: View {
     @State private var isSheetPresented = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
+        ZStack {
+            VStack(alignment: .trailing) {
                 Button(action: {
-                    isSheetPresented.toggle()
+                    print("button action")
                 }) {
-                    Text("Chat with us")
+                    Text("Test")
                         .padding()
                         .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(8)
+                }.padding()
+                Spacer()
+                if isSheetPresented {
+                    viewModel.startSession()
+                        .frame(height: UIScreen.main.bounds.height / 2)
                 }
-                .padding(.horizontal)
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        isSheetPresented.toggle()
+                    }) {
+                        Text("Chat with us")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    .padding(.horizontal)
+                }
             }
         }
-        .fullScreenCover(isPresented: $isSheetPresented, onDismiss: {
-            isSheetPresented = false
-        }, content: {
-            viewModel.startSession() 
-        })
         .onChange(of: viewModel.isReady) {
             if viewModel.isReady {
                 print("Chat is ready")
